ee70a013c00f8e81949550c846783d0debf11548
GuoBaoru <guobaoru@renrenche.com>
2017-08-30 15:15:18 +0800

    测试回滚

--- springboot/src/main/java/com/test/main/Application.java
+++ springboot/src/main/java/com/test/main/Application.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Application {
     @RequestMapping("/")
     public String home() {
+	System.out.println("测试git回滚");
         return "Hello Spring Boot!";
     }
     public static void main(String[] args) {
